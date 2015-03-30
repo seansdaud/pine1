3864730f1c19b4998268668744a00494a17155c2
pracanda <prachanda.gurung@gmail.com>
2015-03-30 17:01:33 +0545

    arena updated

--- app/views/backend/owners/edit_arena.blade.php
+++ app/views/backend/owners/edit_arena.blade.php
@@ -4,13 +4,22 @@
 	<form action="{{ URL::route('arena-info-edited') }}" method="post">
 		<div class="field">
 			enter title:<input type="text" name="name" value="{{ $info->name }}" >
+			@if($errors->has('name'))
+				{{ $errors->first('name') }}
+			@endif
 		</div>
 		<div class="field">
 			Address:<input type="text" name="address" value="{{ $info->address }}">
 		</div>
+		@if($errors->has('address'))
+				{{ $errors->first('address') }}
+			@endif
 		<div class="field">
 			Telephone:<input type="number" name="phone" value="{{ $info->phone }}">
 		</div>
+		@if($errors->has('phone'))
+				{{ $errors->first('phone') }}
+			@endif
 		<div class="field">
 			description:<input type="text" name="about" value="{{ $info->about }}" name="arena_id">
 		</div>
