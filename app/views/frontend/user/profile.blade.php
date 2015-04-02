86b54d7e1ea0ff01878825dc384b8c5a9ee84665
pracanda <prachanda.gurung@gmail.com>
2015-04-02 17:26:18 +0545

    updated profile.user

--- app/views/frontend/user/profile.blade.php
+++ app/views/frontend/user/profile.blade.php
@@ -47,13 +47,13 @@
 							<img class="img-profile img-circle" src="{{ asset('assets/img/5457227d9719a.jpg') }}" alt="" width="50" height="50">
 						@endif
 					</a>
-					<? $arena=Arena::where('id','=', $book->arena_id)->firstOrFail();?>
+					<? $arena=User::where('id','=', $book->arena_id)->firstOrFail();?>
 					<? $time=Scheduleinfo::where('booking_id','=',$book->id)->firstOrFail();?>
 					<div class="comment-body">
 						<div class="text">
-						  <p>You booked <a href="#">{{ $arena->name }}</a> from {{ $time->start_time }} to {{$time->end_time}}</p>
+						  <p>You booked <a href="#">{{ $arena->arenas->name }}</a> from {{ $time->start_time }} to {{$time->end_time}}  for {{$book->booking_date}}</p>
 						</div>
-						<p class="attribution">On {{ date("H:m:s", strtotime($book->created_at)); }}, {{ $book->booking_date }}</p>
+						<p class="attribution">On {{ date("H:m:s", strtotime($book->created_at)); }}, {{ date("Y-M-d", strtotime($book->created_at)) }}</p>
 					</div>
 				</article>
 				@endforeach
