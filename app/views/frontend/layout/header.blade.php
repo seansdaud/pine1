d48f6b69b0ff0ff4b5b89d7f12b013f0e1bc5275
pracanda <prachanda.gurung@gmail.com>
2015-04-16 12:26:17 +0545

    pulllll

--- app/views/frontend/layout/header.blade.php
+++ app/views/frontend/layout/header.blade.php
@@ -12,7 +12,9 @@
 	</title>
 	{{ HTML::style('assets/css/bootstrap.css') }}
 	{{ HTML::style('assets/css/style1.css') }}
+
 	{{ HTML::style('assets/css/select2.min.css') }}
+	{{ HTML::style('assets/css/sweet-alert.css') }}
 	{{ HTML::style('http://fonts.googleapis.com/css?family=Ropa+Sans') }}
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
